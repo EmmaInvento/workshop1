56c6e50a202fd3b1fac97ba8756bc281524809a5
EmmaInvento <emma.invento.01@gmail.com>
2026-09-18 18:58:46 +0200

    last version

--- src/project_1/chat.py
+++ src/project_1/chat.py
@@ -2,7 +2,7 @@ from .client import call_model
 from .prompts.loader import load_prompt
 from .tracing import get_tracer
 
-OFFICES = [] #In the future we might have to classify the office the custumer has to contact
+#OFFICES = [] #In the future we might have to classify the office the custumer has to contact
 
 tracer = get_tracer(__name__)
 
@@ -17,7 +17,7 @@ def chat_response(message:str) -> str:
             instructions = instructions, 
             user_input = message,
             )
-        span.set_attribute("response", response)
+        span.set_attribute("response", response)                                 
         return response
     
 
